0d8ec3b06e21376a2a191727989fb8d76dcfcf6e
Sachin Sudheer <sachin.sudheer301@gmail.com>
2024-08-26 11:26:30 -0600

    102

---
+++ src/main/python/BinaryTreeLevelOrderTraversal_102.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        queue = deque()
+        queue.append(root)
+        while queue:
+            level = []
+            qlen = len(queue)
+            for i in range(qlen):
+                node = queue.popleft()
+                if node:
+                    level.append(node.val)
+                    queue.append(node.left)
+                    queue.append(node.right)
+            if level:
+                res.append(level)
+        return res
