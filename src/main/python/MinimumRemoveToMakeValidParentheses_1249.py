a5bbb8192672940f1f6fc56b9e6b48cc8877a899
Sachin Sudheer <sachin.sudheer301@gmail.com>
2024-09-05 20:16:36 -0600

    1249 update

--- src/main/python/MinimumRemoveToMakeValidParentheses_1249.py
+++ src/main/python/MinimumRemoveToMakeValidParentheses_1249.py
@@ -1,6 +1,21 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         stack = []
+        s = list(s)
+        for i,c in enumerate(s):
+            if c == "(":
+                stack.append(i)
+            elif c == ")":
+                if stack:
+                    stack.pop()
+                else:
+                    s[i] = ''
+        while(stack):
+            s[stack.pop()] = ''
+        return ''.join(s)
+
+    def minRemoveToMakeValid_non_optimized(self, s: str) -> str:
+        stack = []
         res = ''
         count = 0
         for c in s:
