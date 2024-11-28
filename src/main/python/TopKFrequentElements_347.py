b4c7ac1c47f2cc44d207f2b81c129729423044b9
Sachin Sudheer <sachin.sudheer301@gmail.com>
2024-11-27 19:24:29 -0700

    271, 347

--- src/main/python/TopKFrequentElements_347.py
+++ src/main/python/TopKFrequentElements_347.py
@@ -1,4 +1,4 @@
-from collections import Counter
+from collections import Counter, defaultdict
 from typing import List
 
 
@@ -10,3 +10,20 @@ class Solution:
         for i in range(k):
             res.append(freq[i][0])
         return res
+
+    #bucket sort
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = defaultdict(int)
+        freq = [[] for _ in range(len(nums) + 1)]
+        for n in nums:
+            count[n] += 1
+        for n, c in count.items():
+            freq[c].append(n)
+        res = []
+        for i in range(len(freq) - 1, -1, -1):
+            for n in freq[i]:
+                res.append(n)
+                k -= 1
+                if k == 0:
+                    return res
+        return
