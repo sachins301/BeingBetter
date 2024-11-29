d979f0596254bbd2434a0cb2a57368f7906c2162
Sachin Sudheer <sachin.sudheer301@gmail.com>
2024-11-28 19:56:58 -0700

    128

--- src/main/python/LongestConsecutiveSequence_128.py
+++ src/main/python/LongestConsecutiveSequence_128.py
@@ -2,7 +2,30 @@ from typing import List
 
 
 class Solution:
+
+    #DP / Memoization
     def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        res = 0
+        numSet = set(nums)
+        cache = {}
+
+        def get_streak(n):
+            if n not in numSet:
+                return 0
+            if n in cache:
+                return cache[n]
+            cache[n] = 1 + get_streak(n + 1)
+            return cache[n]
+
+        for n in nums:
+            if n - 1 not in numSet:
+                res = max(res, get_streak(n))
+        return res
+
+    # O(n)
+    def longestConsecutive2(self, nums: List[int]) -> int:
         if not nums:
             return 0
         dic = set(nums)
