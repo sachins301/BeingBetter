4bf1fba86a06cd84a7780e4543365171c8055b64
Sachin Sudheer <sachin.sudheer301@gmail.com>
2024-12-01 23:38:24 -0700

    424

--- src/main/python/LongestRepeatingCharacterReplacement_424.py
+++ src/main/python/LongestRepeatingCharacterReplacement_424.py
@@ -12,3 +12,21 @@ class Solution:
                 l += 1
             res = max(res, r - l + 1)
         return res
+
+    # Optimized
+    def characterReplacement(self, s: str, k: int) -> int:
+        visit = defaultdict(int)
+        l = 0
+        r = 0
+        res = 0
+        maxf = 0
+        for r in range(len(s)):
+            visit[s[r]] += 1
+            maxf = max(maxf, visit[s[r]])
+            while (r - l + 1) - maxf > k:
+                visit[s[l]] -= 1
+                l += 1
+
+            res = max(res, r - l + 1)
+        return res
+
