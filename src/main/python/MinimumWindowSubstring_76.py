54a641f5a2d798b5693a29f713e925b91b7df157
Sachin Sudheer <sachin.sudheer301@gmail.com>
2024-12-03 21:42:56 -0700

    76

--- src/main/python/MinimumWindowSubstring_76.py
+++ src/main/python/MinimumWindowSubstring_76.py
@@ -1,8 +1,33 @@
-from collections import Counter
+from collections import Counter, defaultdict, deque
 
 
 class MinimumWindowSubstring_76:
+
     def minWindow(self, s: str, t: str) -> str:
+        res = ""
+        l = 0
+        window = defaultdict(int)
+        queue = deque()
+        tCount = Counter(t)
+        need, have = len(tCount), 0
+        for i in range(len(s)):
+            if s[i] not in t:
+                continue
+            window[s[i]] += 1
+            queue.append(i)
+            if window[s[i]] == tCount[s[i]]:
+                have += 1
+            while have == need:
+                l = queue[0]
+                if i - l + 1 < len(res) or not res:
+                    res = s[l : i+1]
+                window[s[l]] -= 1
+                if window[s[l]] < tCount[s[l]]:
+                    have -= 1
+                queue.popleft()
+        return res
+
+    def minWindow2(self, s: str, t: str) -> str:
         if len(t) > len(s):
             return ""
 
