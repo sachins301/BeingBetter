3b6cb59a6c9c5e80aee1125faaefb8887c35d6e7
Sachin Sudheer <sachin.sudheer301@gmail.com>
2024-12-04 23:47:26 -0700

    853, 739

--- src/main/python/CarFleet_853.py
+++ src/main/python/CarFleet_853.py
@@ -9,3 +9,17 @@ class CarFleet_853:
                 stack.append(time)
 
         return len(stack)
+
+    def carFleet_2(self, target: int, position: List[int], speed: List[int]) -> int:
+        # 10-1, 8-1, 0-12, 5-7, 3-3
+        # 0-12, 3-3, 5-7, 8-1, 10-1
+
+        pairs = [[position[i], (target - position[i])/speed[i]] for i in range(len(position))]
+        pairs.sort()
+        stack = []
+        for p in pairs:
+            while stack and p[1] >= stack[-1][1]:
+                stack.pop()
+            stack.append(p)
+            # print(stack)
+        return len(stack)
--- src/main/python/DailyTemperatures_739.py
+++ src/main/python/DailyTemperatures_739.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class DailyTemperatures_739:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         stack = [] # [temp, index]
@@ -10,4 +13,18 @@ class DailyTemperatures_739:
                 stack.pop()
 
             stack.append([temperatures[i], i])
+        return res
+
+    def dailyTemperatures_dp(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        res = [0] * n
+        for i in range(n - 2, -1, -1):
+            j = i + 1
+            while j < n and temperatures[j] <= temperatures[i]:
+                if res[j] == 0:
+                    j = n
+                    break
+                j += res[j]
+            if j < n:
+                res[i] = j - i
         return res
