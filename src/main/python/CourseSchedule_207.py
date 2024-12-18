5a1542a485acc8f0bc84058114a67a1c7b8e32f4
Sachin Sudheer <sachin.sudheer301@gmail.com>
2024-12-17 21:31:13 -0700

    207

--- src/main/python/CourseSchedule_207.py
+++ src/main/python/CourseSchedule_207.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import List
 
 
@@ -25,3 +26,24 @@ class Solution:
             if not dfs(crs):
                 return False
         return True
+
+
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        prereq = [0] * numCourses
+        prereqlist = [[] for i in range(numCourses)]
+        for src, dst in prerequisites:
+            prereq[dst] += 1
+            prereqlist[src].append(dst)
+        q = deque()
+        for i in range(numCourses):
+            if prereq[i] == 0:
+                q.append(i)
+        finish = 0
+        while q:
+            crs = q.popleft()
+            finish += 1
+            for nei in prereqlist[crs]:
+                prereq[nei] -= 1
+                if prereq[nei] == 0:
+                    q.append(nei)
+        return finish == numCourses
