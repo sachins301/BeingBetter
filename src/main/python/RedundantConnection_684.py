d5ba6c8bd0de82b103704b1cc46df183d91d3707
Sachin Sudheer <sachin.sudheer301@gmail.com>
2024-12-20 11:26:29 -0700

    684

--- src/main/python/RedundantConnection_684.py
+++ src/main/python/RedundantConnection_684.py
@@ -2,6 +2,43 @@ from typing import List
 
 
 class Solution:
+
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        adj = [[] for i in range(n+1)]
+        for s,d in edges:
+            adj[s].append(d)
+            adj[d].append(s)
+        visit = set()
+        cycle = set()
+        res = []
+        cyclestart = [-1]
+        def dfs(node, parent):
+            if node in visit:
+                cyclestart[0] = node
+                cycle.add(node)
+                return True
+            visit.add(node)
+            for nei in adj[node]:
+                if nei == parent:
+                    continue
+                if dfs(nei, node):
+                    if cyclestart[0] != -1:
+                        cycle.add(node)
+                        if node == cyclestart[0]:
+                            cyclestart[0] = -1
+                    return True
+            return False
+
+        dfs(1, -1)
+
+        for s, d in edges[::-1]:
+            if s in cycle and d in cycle:
+                return [s, d]
+
+
+
+
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         parent = [i for i in range(len(edges)+1)]
         rank = [1] * (len(edges)+1)
