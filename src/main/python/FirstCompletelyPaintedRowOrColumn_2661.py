666326fa444164eed527a2623a46c704e701a7c8
Sachin Sudheer <sachin.sudheer301@gmail.com>
2025-01-19 23:40:58 -0700

    2661

---
+++ src/main/python/FirstCompletelyPaintedRowOrColumn_2661.py
@@ -0,0 +1,16 @@
+class Solution:
+    def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
+        paintedrow = defaultdict(int)
+        paintedcol = defaultdict(int)
+        m, n = len(mat), len(mat[0])
+        position = {}
+        for i in range(m):
+            for j in range(n):
+                position[mat[i][j]] = (i, j)
+        for i in range(len(arr)):
+            r, c = position[arr[i]]
+            paintedrow[r] += 1
+            paintedcol[c] += 1
+            if paintedrow[r] == n or paintedcol[c] == m:
+                return i
+        return -1
